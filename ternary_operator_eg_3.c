270d32360287ab3199281bd857849c1e76939156
Sampaul Kinyua <sincerityeightny@gmail.com>
2025-08-27 18:36:41 +0700

    Add files via upload

---
+++ ternary_operator_eg_3.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+#include <stdbool.h>
+int main() {
+    
+    // ternary operator ? = shorthand for if-else statement
+
+    //(condition) ? value_if_true : value_if_false;
+
+int age = 21;
+
+printf("%s", (age > 18) ? "adult" : "child");
+
+
+
+    return 0;
+}
